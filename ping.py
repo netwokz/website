34fc01122862b084731d3d04af83fafbafb20132
Steve Deane Jr <wildmechanic@gmail.com>
2022-07-23 13:17:46 -0700

    update

--- ping.py
+++ ping.py
@@ -1,23 +1,28 @@
+from http import server
 import os
 import subprocess
+import json
 
-ip_list = {'TrueNas': '10.10.10.10',
-           'Desktop': '10.10.10.9', 'Torrent-box': '10.10.10.206'}
-# , "10.10.10.43", "10.10.10.129", "10.10.10.15", "10.10.10.8", "10.10.10.11", "10.10.10.206"}
-# ip_list = ["10.10.10.10"]
+from cairo import Status
 
-count = 2
+count = 1
 
-# for ip in ip_list:
-#     response = os.popen(f"ping -c {count} -W 3 {ip}").read()
-#     if f"{count} received" in response:
-#         print(f"{ip} is UP")
-#     else:
-#         print(f"{ip} is DOWN")
+ip_list = {'TrueNas': ['10.10.10.10', 'DOWN'],
+           'Desktop': ['10.10.10.9', 'DOWN'],
+           'Torrent-box': ['10.10.10.206', 'DOWN'],
+           'Plex': ['10.10.10.43', 'DOWN'],
+           'HomeAssistant': ['10.10.10.7', 'DOWN'],
+           'InfluxDB': ['10.10.10.13', 'DOWN'],
+           'Portainer': ['10.10.10.8', 'DOWN'],
+           'PiHole': ['10.10.10.2', 'DOWN'],
+           'Mikrotik': ['10.10.10.3', 'DOWN']}
 
-for key, value in ip_list.items():
-    response = os.popen(f"ping -c {count} -W 3 {value}").read()
+for key, item in ip_list.items():
+    response = os.popen(f"ping -c {count} -W 3 {item[0]}").read()
     if f"{count} received" in response:
-        print(f"{key} is UP")
+        ip_list[key][1] = 'UP'
     else:
-        print(f"{key} is DOWN")
+        ip_list[key][1] = 'DOWN'
+
+with open('convert.txt', 'w') as convert_file:
+    convert_file.write(json.dumps(ip_list))
--- website/views.py
+++ website/views.py
@@ -1,8 +1,17 @@
+from fileinput import filename
 from flask import Blueprint, render_template
+import ast
 
 views = Blueprint('views', __name__)
 
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    # out = open(r'ping.py', 'r').read()
+    # exec(out)
+
+    with open("convert.txt", "r") as data:
+        dictionary = ast.literal_eval(data.read())
+        # print(dictionary)
+
+    return render_template('home.html', dictionary=dictionary)
